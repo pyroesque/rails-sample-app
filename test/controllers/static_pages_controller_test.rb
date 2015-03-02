c8a53de884063006a79f50bedaee4413007af244
Chris Jones <cdjones42@gmail.com>
2015-03-02 10:28:05 +0900

    added contacts page, added full_title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,30 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @title_string = "Ruby on Rails Tutorial Sample App"
+  end
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "#{@title_string}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | #{@title_string}"
   end
 
   test "should get about" do 
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | #{@title_string}"
+  end
+
+  test "should get contacts" do
+    get :contacts
+    assert_response :success
+    assert_select "title", "Contacts | #{@title_string}"
   end
 end
